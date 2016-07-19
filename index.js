380235d392cfb43556504039911c74a3a2c74ed1
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-19 22:10:06 +1200

    some refactoring, show error messages

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 var BinaryXHR = require('binary-xhr')
 var toBase64 = require('arraybuffer-base64')
 
+var Progress = require('hyperprogress')
+
+var prog = Progress()
+var running = false
+
+window.WebBoot = {
+  scorchedEarth: scorchedEarth,
+  reinitialize: reinitialize,
+  add: add,
+  run: run
+}
+
+function parse (str) {
+  try {
+    return JSON.parse(str)
+  } catch (_) { }
+}
+
+document.body.appendChild(prog)
+
 //split the hash.
 
 var parts = window.location.hash.split('#').slice(1)
@@ -8,6 +28,7 @@ var hasHash = /([A-Za-z0-9\/+]{43}=)\.sha256/
 var isUrl = /^https?:\/\//
 
 var APPNAME = 'SWB'
+var match = new RegExp('^'+APPNAME)
 
 function log (msg) {
   console.log(APPNAME, msg)
@@ -26,13 +47,31 @@ function hash(data, cb) {
   })
 }
 
-function run (id) {
+function reinitialize () {
+  for(var k in localStorage) {
+    if(match.test(k))
+      delete localStorage[k]
+  }
+}
+
+//destroy everything
+function scorchedEarth () {
+  for(var k in localStorage) {
+    delete localStorage[k]
+  }
+}
+
+function run (id, cb) {
+  //if we are already running, restart
+  if(running) {
+    //leave a note in local storage, and restart.
+    location.hash = '#run:'+id
+  }
   var text = localStorage[APPNAME+'_version_'+id]
-  var buffer = new TextEncoder('utf8').encode(text)
-  hash(buffer, function (err, hash) {
-    if(localStorage.SWB_current !== hash) {
+  hash(new TextEncoder('utf8').encode(text), function (err, _id) {
+    if(id !== _id) {
       //LOCAL STORAGE CORRUPTED
-      alert('localStorage is corrupted')
+      cb(new Error('localStorage is corrupted'))
     }
     else {
       var script = document.createElement('script')
@@ -43,39 +82,57 @@ function run (id) {
 
 }
 
+function add (secure_url, cb) {
+  if(!(isUrl.test(secure_url) && hasHash.test(secure_url))
+    return cb(new Error('is not a secure url:'+secure_url))
+  var id = hasHash.exec(parts[0])[1]
+  BinaryXHR(secure_url, function (err, buf) {
+    if(err)
+      return prog.fail(err, 'could not retrive secure url')
+
+    hash(buf, function (err, _id) {
+      if(err)
+        return prog.fail(err, 'hash failed')
+
+      if(id !== _id) {
+        return prog.fail(new Error('secure url is invalid'))
+      }
+      cb(null, _id)
+    })
+  })
+}
+
 if(parts.length && isUrl.test(parts[0]) && hasHash.test(parts[0])) {
   var id = hasHash.exec(parts[0])[1]
-  log('detected secure url:'+ parts[0])
+  prog.next('detected secure url:'+ parts[0])
+
   //check if we already have this data.
   if(localStorage[APPNAME+'_version_'+id]) {
+    prog.next('loading local version')
     run(id)
   } else
-    log('GET:'+parts[0])
-    BinaryXHR(parts[0], function (err, buf) {
-      if(err) {
-        log('did not retrive new version')
-        throw err
-      }
-      hash(buf, function (err, _id) {
-        if(id !== _id) {
-          //ERROR. requested data was corrupt.
-        }
-        else {
-          log('received correct hash:'+id)
-          //returned data is correct. save, then run.
-          localStorage[APPNAME+'_version_'+id] = new TextDecoder('utf8').decode(buf)
-          //reload to run app
-          localStorage[APPNAME+'_current'] = id
-          window.location = '#' + parts.slice(1).join('#')
-          window.location.reload()
-        }
-      })
+    prog.next('retriving secure url:'+parts[0])
+    add(parts[0], function (err, id) {
+      if(err) return prog.fail(err)
+
+      prog.next('loading secure javascript')
+      //returned data is correct. save, then run.
+      localStorage[APPNAME+'_version_'+id] = new TextDecoder('utf8').decode(buf)
+      localStorage[APPNAME+'_current'] = id
+
+      var versions = parse(localStorage[APPNAME+'_versions']) || {}
+      versions[Date.now()] = id
+      localStorage[APPNAME+'_versions'] = JSON.stringify(versions)
+
+      window.location = '#' + parts.slice(1).join('#')
+      window.location.reload()
+
     })
 }
 else if(localStorage[APPNAME+'_current']) {
   run(localStorage[APPNAME+'_current'])
 }
 else {
-  alert('nothing to run')
+  prog.next('no code to run: paste #{secure_url} to a javascript file.')
 }
 
