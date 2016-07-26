f69a97fab098649ab4f677a90c63f5cc9e732665
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-26 13:22:53 +1200

    include a minimal server

---
+++ handler.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+var path = require('path')
+
+var index = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8')
+var hash = require('crypto').createHash('sha256').update(index, 'utf8').digest('base64')
+
+var manifest = 'CACHE MANIFEST\n/\n#' + hash + '\n'
+module.exports = function (req, res, next) {
+  console.log(req.method, req.url)
+  if(req.url == '/')
+    res.end(index)
+  else if(req.url == '/manifest.appcache')
+    res.end(manifest)
+  else
+    res.writeHead(200)
+}
+
+
+
+if(!module.parent)
+  require('http').createServer(module.exports).listen(4789)
+
+
