393889cb90e03bdbaf76a4f5085ed7c1958932a2
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-29 18:05:15 +1200

    static, appcache generator

---
+++ handler/cache.js
@@ -0,0 +1,13 @@
+module.exports = function (index) {
+  return 'CACHE MANIFEST\n/\n#' +
+    require('crypto')
+      .createHash('sha256')
+      .update(index, 'utf8')
+      .digest('base64') + '\n'
+}
+
+if(!module.parent)
+  console.log(module.exports(
+    require('fs')
+      .readFileSync(require('path').join(__dirname, '..', 'index.html'))
+  ))
--- handler/index.js
+++ handler/index.js
@@ -2,11 +2,10 @@ var fs = require('fs')
 var path = require('path')
 
 var index = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8')
-var hash = require('crypto').createHash('sha256').update(index, 'utf8').digest('base64')
 
-var manifest = 'CACHE MANIFEST\n/\n#' + hash + '\n'
+var manifest = require('./cache')(index)
+
 module.exports = function (req, res, next) {
-  console.log(req.method, req.url)
   if(req.url == '/')
     res.end(index)
   else if(req.url == '/manifest.appcache')
@@ -16,10 +15,10 @@ module.exports = function (req, res, next) {
   else res.writeHead(404), res.end('404!')
 }
 
-
-
 if(!module.parent)
   require('http').createServer(module.exports).listen(45789)
+
+
 
 
 
