34e7c00e0ae76de342a2c5f3ff40e73072483457
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-31 18:58:53 +1200

    explicitly set most things as not cached

--- handler/cache.js
+++ handler/cache.js
@@ -1,5 +1,5 @@
 module.exports = function (index) {
-  return 'CACHE MANIFEST\n\n#' +
+  return 'CACHE MANIFEST\n\nNETWORK:\n*\n#' +
     require('crypto')
       .createHash('sha256')
       .update(index, 'utf8')
