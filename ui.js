af10df17c2bfd1cf296e1800fa5c7454399d79e2
Dominic Tarr <dominic.tarr@gmail.com>
2016-09-19 09:38:13 +1200

    handle opening page with secure url in hash

--- ui.js
+++ ui.js
@@ -3,10 +3,7 @@ var input_file = require('hyperfile')
 var Progress = require('hyperprogress')
 var u = require('./util')
 var prog = Progress()
-
-//split the hash.
-var parts = window.location.hash.split('#').slice(1)
-var QUOTA = 5*1024*1024
+var SecureUrl = require('./fetch')
 
 var human = require('human-time')
 
@@ -28,105 +25,117 @@ module.exports = function (appname, wb) {
     return (Math.round((bytes / 1024) * 100)/100) + 'k'
   }
 
-//  var isElectron = typeof process !== 'undefined'
-//      && process.env && process.env[appname+'_INIT']
+  function logFail(err) {
+    if(err) throw prog.fail(err)
+  }
 
   function handleQuota(err, data, id) {
     if(err
-      && err.name == "QuotaExceededError"
+      && /quota/i.test(err.name)
       && confirm ('adding: '+id+' exceedes quota, clear cache?')
     ) {
-      wb.prune(data.length, function (err) {
+      wb.prune(data.length || data.byteLength || 0, function (err) {
         if(err) throw prog.fail(err)
         wb.add(data, id, function (err) {
           if(err) throw prog.fail(err)
-          wb.run(id, function (err) {
-            if(err) throw prog.fail(err)
-          })
+          wb.run(id, logFail)
         })
       })
     }
-    else if(!err) wb.run(id, function (err) {
-        if(err) throw prog.fail(err)
-      })
+    else if(!err) wb.run(id, logFail)
     else
       throw prog.fail(err)
   }
 
+  function clearHash (err) {
+    if(err) throw prog.fail(err)
+    else location.hash = location.hash.split('#').splice(2).join('#')
+  }
+
   ;(function redraw () {
+    //okay, check if the hash has a secure url in it first.
+    var url = location.hash.split('#').splice(1).shift()
+    var id = SecureUrl.isSecureUrl(url)
+    if(!id) url = null
     wb.versions(function (err, log) {
       if(err) throw prog.fail(err)
       document.body.innerHTML = ''
-      if(log.length && !wb.isInit())
-        return wb.run(log[0].value, function (err) {
-          if(err) throw prog.fail(err)
-        })
-
-      document.body.appendChild(
-        h('div', {classList: 'WebBoot'}, [
-          ( !log.length
-          ? h('h2', 'please enter secure-url or select file to run')
-          : h('ol', {classList: 'WebBoot__recent'}, log.map(function (v) {
-              return h('li', [
-                h('code', [v.value]),
-                h('label',
-                  { title: new Date(v.ts).toString() },
-                  [ ' (loaded ', human(new Date(v.ts)), ') ']
-                ),
-                h('div', [
-                  btn('run', function () {
-                    wb.run(v.value)
-                  }),
-                  btn('revert', function () {
-                    wb.revert(v.ts, function (err) {
-                      if(err) throw prog.fail(err)
-                      redraw()
+      //if we have a version ready, and there is no url provided, run it.
+      if(!wb.isInit()) {
+        //if there is no logged versions, run the url version
+        if(id && !log.length) {
+          wb.installAndRun(url, clearHash)
+        }
+        //if there is no url, run the log version
+        else if(log.length && !id)
+          wb.run(log[0].value, logFail)
+        //prompt if they want to upgrade to new url version.
+        else if(id && log.length) {
+          //if the url is the same as we would have run,
+          //then just run it.
+          if(log[0].value === id)
+            wb.run(log[0].value, clearHash)
+          else if(confirm('change version to:'+url))
+            wb.installAndRun(url, clearHash)
+          else
+            wb.run(log[0].value, logFail)
+        }
+      }
+      else
+        document.body.appendChild(
+          h('div', {classList: 'WebBoot'}, [
+            ( !log.length
+            ? h('h2', 'please enter secure-url or select file to run')
+            : h('ol', {classList: 'WebBoot__recent'}, log.map(function (v) {
+                return h('li', [
+                  h('code', [v.value]),
+                  h('label',
+                    { title: new Date(v.ts).toString() },
+                    [ ' (loaded ', human(new Date(v.ts)), ') ']
+                  ),
+                  h('div', [
+                    btn('run', function () {
+                      wb.run(v.value)
+                    }),
+                    btn('revert', function () {
+                      wb.revert(v.ts, function (err) {
+                        if(err) throw prog.fail(err)
+                        redraw()
+                      })
                     })
-                  })
+                  ])
                 ])
-              ])
-            }))
-          ),
+              }))
+            ),
 
-          //paste a secure url into this text input
-          h('input', {
-            placeholder: 'enter secure url',
-            onchange: function (ev) {
-              //else, download it. if that succeeds,
-              //add to store, if success, run.
-              //if that fails, offer to clean up, or fail.
-              //add to store, if success, run.
+            //paste a secure url into this text input
+            h('input', {
+              placeholder: 'enter secure url',
+              onchange: function (ev) {
+                //else, download it. if that succeeds,
+                //add to store, if success, run.
+                //if that fails, offer to clean up, or fail.
+                //add to store, if success, run.
 
-              //this can fail from quota exceeded.
-              //check whether we have this already, if so, run it.
-              var url = ev.target.value
+                //this can fail from quota exceeded.
+                //check whether we have this already, if so, run it.
+                var url = ev.target.value
 
-              wb.install(url, handleQuota)
-            }
-          }),
+                wb.install(url, handleQuota)
+              }
+            }),
 
-          //or select a local file to run
-          input_file(function (data) {
-            wb.add(data, function (err, id) {
-              handleQuota(err, data, id)
+            //or select a local file to run
+            input_file(function (data) {
+              wb.add(data, function (err, id) {
+                if(err) console.log(err.name)
+                handleQuota(err, data, id)
+              })
             })
-          })
-        ])
-      )
+          ])
+        )
     })
   })()
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
